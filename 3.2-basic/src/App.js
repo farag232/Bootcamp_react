390796ea666c21be5191d81208c3cf27a6634ea7
farag232 <faragfadool232@gmail.com>
2022-11-03 11:06:55 +0200

    second message

---
+++ 3.2-basic/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  const data = ["Hello", "World"];
+  const number1 = 5;
+  const number2 = 6;
+  const string = "I love React!";
+  return (
+    <div>
+       <div>{data.join(" ")}</div>
+      <div>{`${number1} + ${number2} = ${number1 + number2}`}</div>
+      <div>The string’s length is {string.length}</div>
+    </div>
+  );
+}
+
+export default App;
